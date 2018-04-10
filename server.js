7ea28ada2274cb238b7f8626defe424bf4b224c0
aneagoie <aneagoie1@gmail.com>
2018-04-10 12:35:58 -0400

    FEATURE: adding ability to update profile

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ app.use(bodyParser.json());
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
+app.post('/profile/:id', (req, res) => { profile.handleProfileUpdate(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
