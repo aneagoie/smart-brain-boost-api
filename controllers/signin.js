879755fa4f0157a715ffd58aa310f3ad26a0b662
aneagoie <aneagoie1@gmail.com>
2018-04-11 18:52:54 -0400

    FEATURE: adding JWT authorization

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,26 +1,69 @@
-const handleSignin = (db, bcrypt) => (req, res) => {
+const jwt = require('jsonwebtoken');
+
+// Redis Setup
+const redis = require('redis');
+// You will want to update your host to the proper address in production
+const redisClient = redis.createClient({host: '127.0.0.1'});
+
+const signToken = (username) => {
+  const jwtPayload = { username };
+  return jwt.sign(jwtPayload, 'JWT_SECRET_KEY', { expiresIn: '2 days'});
+};
+
+const setToken = (key, value) => Promise.resolve(redisClient.set(key, value));
+
+const createSession = (user) => {
+  const { email, id } = user;
+  const token = signToken(email);
+  return setToken(token, id)
+    .then(() => {
+      return { success: 'true', userId: id, token, user }
+    })
+    .catch(console.log);
+};
+
+const handleSignin = (db, bcrypt, req, res) => {
   const { email, password } = req.body;
   if (!email || !password) {
-    return res.status(400).json('incorrect form submission');
+    return Promise.reject('incorrect form submission');
   }
-  db.select('email', 'hash').from('login')
+  return db.select('email', 'hash').from('login')
     .where('email', '=', email)
     .then(data => {
       const isValid = bcrypt.compareSync(password, data[0].hash);
       if (isValid) {
         return db.select('*').from('users')
           .where('email', '=', email)
-          .then(user => {
-            res.json(user[0])
-          })
+          .then(user => user[0])
           .catch(err => res.status(400).json('unable to get user'))
       } else {
-        res.status(400).json('wrong credentials')
+        return Promise.reject('wrong credentials');
       }
     })
-    .catch(err => res.status(400).json('wrong credentials'))
+    .catch(err => err)
+}
+
+const getAuthTokenId = (req, res) => {
+  const { authorization } = req.headers;
+  return redisClient.get(authorization, (err, reply) => {
+    if (err || !reply) {
+      return res.status(401).send('Unauthorized');
+    }
+    return res.json({id: reply})
+  });
+}
+
+const signinAuthentication = (db, bcrypt) => (req, res) => {
+  const { authorization } = req.headers;
+  return authorization ? getAuthTokenId(req, res)
+    : handleSignin(db, bcrypt, req, res)
+    .then(data =>
+      data.id && data.email ? createSession(data) : Promise.reject(data))
+    .then(session => res.json(session))
+    .catch(err => res.status(400).json(err));
 }
 
 module.exports = {
-  handleSignin: handleSignin
+  signinAuthentication: signinAuthentication,
+  redisClient: redisClient
 }
--- server.js
+++ server.js
@@ -8,7 +8,9 @@ const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
+const auth = require('./controllers/authorization');
 
+//Database Setup
 const db = knex({
   client: 'pg',
   connection: {
@@ -24,14 +26,12 @@ const app = express();
 app.use(cors())
 app.use(bodyParser.json());
 
-// This line is no longer needed:
-// app.get('/', (req, res)=> { res.send(database.users) })
-app.post('/signin', signin.handleSignin(db, bcrypt))
+app.post('/signin', signin.signinAuthentication(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
-app.post('/profile/:id', (req, res) => { profile.handleProfileUpdate(req, res, db)})
-app.put('/image', (req, res) => { image.handleImage(req, res, db)})
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+app.get('/profile/:id', auth.requireAuth, (req, res) => { profile.handleProfileGet(req, res, db)})
+app.post('/profile/:id', auth.requireAuth, (req, res) => { profile.handleProfileUpdate(req, res, db)})
+app.put('/image', auth.requireAuth, (req, res) => { image.handleImage(req, res, db)})
+app.post('/imageurl', auth.requireAuth, (req, res) => { image.handleApiCall(req, res)})
 
 app.listen(3000, ()=> {
   console.log('app is running on port 3000');
